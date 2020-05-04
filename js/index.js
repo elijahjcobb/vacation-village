14a7bb164833bbff3e075863fdc7127f01bdb9d4
Elijah Cobb <elijah@elijahcobb.com>
2020-05-04 11:39:37 -0400

    early version

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var linksInNav = document.querySelectorAll("nav a");
+var _loop_1 = function (link) {
+    link.onclick = function () {
+        var id = link.innerHTML.toLowerCase();
+        var elementToLink = document.getElementById(id);
+        var pos = elementToLink.offsetTop - 160;
+        if (id === "home")
+            pos = 0;
+        window.scrollTo({ top: pos });
+    };
+};
+for (var _i = 0, linksInNav_1 = linksInNav; _i < linksInNav_1.length; _i++) {
+    var link = linksInNav_1[_i];
+    _loop_1(link);
+}
+document.getElementById("learnMoreButton").onclick = function () {
+    var elementToLink = document.getElementById("about");
+    var pos = elementToLink.offsetTop - 160;
+    window.scrollTo({ top: pos });
+};
---
+++ js/index.ts
@@ -0,0 +1,19 @@
+const linksInNav = document.querySelectorAll("nav a") as unknown as HTMLLinkElement[];
+
+for (const link of linksInNav)  {
+	link.onclick = (): void => {
+
+		const id: string = link.innerHTML.toLowerCase();
+		const elementToLink = document.getElementById(id) as HTMLDivElement;
+		let pos = elementToLink.offsetTop - 160;
+		if (id === "home") pos = 0;
+		window.scrollTo({top: pos});
+
+	};
+}
+
+document.getElementById("learnMoreButton").onclick = (): void => {
+	const elementToLink = document.getElementById("about") as HTMLDivElement;
+	let pos = elementToLink.offsetTop - 160;
+	window.scrollTo({top: pos});
+}
