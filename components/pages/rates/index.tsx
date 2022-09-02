642fbbf10896c33e0036ed23179e2e37c0592f53
Elijah Cobb <elijah.cobb@vercel.com>
2022-09-02 14:33:26 -0400

    work on contact and colors

--- components/pages/rates/index.tsx
+++ components/pages/rates/index.tsx
@@ -1,10 +1,21 @@
 import { Container } from "../../shell/max-width-container";
-import styles from "./index.module.css";
 
 export function RatesPage() {
 	return <Container>
-		<h1>Rates</h1>
-		<p>Vacation Village lots range in price from $371 to $417* per month, depending on the location of the lot.</p>
-		<p>*These prices do not include the Michigan Mobile Home Tax of $3.00 per month when occupied.</p>
+		<section>
+			<h1>Rates</h1>
+			<p>Vacation Village lots range in price from $371 to $417* per month, depending on the location of the lot.</p>
+			<p>*These prices do not include the Michigan Mobile Home Tax of $3.00 per month when occupied.</p>
+		</section>
+		<section>
+			<h2>Our rules</h2>
+			<p>If you would like to view our rules and regulations please click the link below to view an updated rulebook.</p>
+			<a target='_blank' href="https://docs.google.com/document/d/18u_k-eS-a9QPYJJi0_csuJM_-XEcIUQFm4rcVmt7Wsk/edit?usp=sharing" rel="noreferrer">View Rules and Regulations</a>
+		</section>
+		<section>
+			<h2>RENTAL APPLICATION</h2>
+			<p>To download our rental application, click the link below. Please make sure that you read our rules and regulations.</p>
+			<a target='_blank' href="/application.pdf">View Rental Application</a>
+		</section>
 	</Container>
 }
