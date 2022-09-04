ba3eea6477525a98cb0476818d4e3316bac8b215
Elijah Cobb <elijah.cobb@vercel.com>
2022-09-03 20:36:05 -0400

    fix homepage

--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -2,7 +2,13 @@ import { Container } from "../../shell/max-width-container";
 import styles from "./index.module.css";
 
 export function HomePage() {
-	return <Container background="/background.jpg">
-		<h1>Vacation Village</h1>
-	</Container>
+	return <Container>
+		<div className={styles.hero}>
+			{/* eslint-disable-next-line @next/next/no-img-element */}
+			<img src="/background.jpg" alt="aerial image" />
+			<div className={styles.dark} />
+			<span>{`Beneath the towering maples on the water's edge, you will love calling Vacation Village your home.`}</span>
+		</div>
+		<div className={styles.padding} />
+	</Container >
 }
